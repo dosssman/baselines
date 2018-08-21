f333a425075f2c8cb8a9851af00fc862e5a83bbb
dosssman <dosssman@hotmail.fr>
2018-08-21 15:02:12 +0900

    Logger set to tensorbard too

--- baselines/logger.py
+++ baselines/logger.py
@@ -8,7 +8,7 @@ import datetime
 import tempfile
 from collections import defaultdict
 
-LOG_OUTPUT_FORMATS     = ['stdout', 'log', 'csv']
+LOG_OUTPUT_FORMATS     = ['stdout', 'log', 'csv', 'tensorboard', "json"]
 LOG_OUTPUT_FORMATS_MPI = ['log']
 # Also valid: json, tensorboard
 
