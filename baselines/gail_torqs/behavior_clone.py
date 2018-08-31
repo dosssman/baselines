21459e811e16b1413ac8ed312162e0b457e86740
dosssman <dosssman@hotmail.fr>
2018-08-31 12:40:09 +0900

    Attempt to fix saving in behavior_cloning

--- baselines/gail_torqs/behavior_clone.py
+++ baselines/gail_torqs/behavior_clone.py
@@ -75,7 +75,9 @@ def learn(env, policy_func, dataset, optim_batch_size=128, max_iters=1e4,
         savedir_fname = tempfile.TemporaryDirectory().name
     else:
         savedir_fname = osp.join(ckpt_dir, task_name)
-    U.save_state(savedir_fname, var_list=pi.get_variables())
+    # U.save_state(savedir_fname, var_list=pi.get_variables())
+    # XXX Just save everything already
+    U.save_state(savedir_fname)
     return savedir_fname
 
 
