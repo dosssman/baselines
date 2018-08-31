61861b58ffeaf8d210f918a4f18e1b117950aea4
dosssman <dosssman@hotmail.fr>
2018-08-31 12:31:55 +0900

    tf_util.py enable save_state()

--- baselines/common/tf_util.py
+++ baselines/common/tf_util.py
@@ -284,7 +284,7 @@ def load_state(fname):
     saver = tf.train.Saver()
     saver.restore(tf.get_default_session(), fname)
 
-# def save_state(fname):
-#     os.makedirs(os.path.dirname(fname), exist_ok=True)
-#     saver = tf.train.Saver()
-#     saver.save(tf.get_default_session(), fname)
+def save_state(fname):
+    os.makedirs(os.path.dirname(fname), exist_ok=True)
+    saver = tf.train.Saver()
+    saver.save(tf.get_default_session(), fname)
