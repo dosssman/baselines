fb0b0a3418cb5ff1886b9b6761fd67210567fff8
Dossa Rousslan Fernand Julien <dosssman@hotmail.fr>
2019-03-26 15:04:06 +0900

    Alpha to 0.4 for retrain, updated alphaplotter to error bar style

--- baselines/remi/alphaplot.py
+++ baselines/remi/alphaplot.py
@@ -58,34 +58,55 @@ if __name__ == '__main__':
 
     print( "Alphas: ", alphas)
     print( "Avg Rets: ", avg_returns)
+    # print( "25th percentile", percentile_25th)
+    # print( "75th percentile", percentile_75th)
+
+    yerr_25th = [ - di + avg_returns[i] for i, di in enumerate(percentile_25th)]
+    yerr_75th = [ di - avg_returns[i] for i, di in enumerate(percentile_75th)]
 
     # Average returns
-    fig1 = plt.figure(1)
-    plt.bar( alphas, avg_returns, align="center", color="blue", label="Avg")
-    plt.title("ReMi - Avg Returns ~ Alphas")
-    plt.xlabel( "Alphas")
-    plt.ylabel( "Scores")
-    plt.legend()
+    # fig1 = plt.figure(1)
+    # plt.bar( alphas, avg_returns, align="center", color="blue", label="Avg")
+    # plt.title("ReMi - Avg Returns ~ Alphas")
+    # plt.xlabel( "Alphas")
+    # plt.ylabel( "Scores")
+    # plt.legend()
 
     # Average Returns * 25/75 percentiles
-    fig2 = plt.figure(2)
-    plt.bar( alphas, percentile_75th, align="center", color="red", label="75th %tile")
-    plt.bar( alphas, avg_returns, align="center", color="blue", label="Avg")
-    plt.bar( alphas, percentile_25th, align="center", color="green", label="25th %tile")
-    plt.title( "ReMi - Avg Returns ~ Alphas - 25th/75th %tile")
-    plt.xlabel( "Alphas")
-    plt.ylabel( "Scores")
-    plt.legend()
+    # fig2 = plt.figure(2)
+    # plt.bar( alphas, percentile_75th, align="center", color="red", label="75th %tile")
+    # plt.bar( alphas, avg_returns, align="center", color="blue", label="Avg")
+    # plt.bar( alphas, percentile_25th, align="center", color="green", label="25th %tile")
+    # plt.title( "ReMi - Avg Returns ~ Alphas - 25th/75th %tile")
+    # plt.xlabel( "Alphas")
+    # plt.ylabel( "Scores")
+    # plt.legend()
 
     # Avg Retusn + 10/90 percentiles
-    fig3 = plt.figure(3)
-    plt.bar( alphas, percentile_90th, align="center", color="red", label="90th %tile" )
-    plt.bar( alphas, avg_returns, align="center", color="blue", label="Avg")
-    plt.bar( alphas, percentile_10th, align="center", color="green", label="10th %tile")
-    plt.title( "ReMi - Avg Returns ~ Alphas - 10th/90th %tile")
+    # fig3 = plt.figure(3)
+    # plt.bar( alphas, percentile_90th, align="center", color="red", label="90th %tile" )
+    # plt.bar( alphas, avg_returns, align="center", color="blue", label="Avg")
+    # plt.bar( alphas, percentile_10th, align="center", color="green", label="10th %tile")
+    # plt.title( "ReMi - Avg Returns ~ Alphas - 10th/90th %tile")
+    # plt.xlabel( "Alphas")
+    # plt.ylabel( "Scores")
+    # plt.legend()
+
+    # With Error bar
+    fig4 = plt.figure(4)
+    plt.errorbar( alphas, avg_returns, yerr=[yerr_25th,yerr_75th], fmt="--o",
+        ecolor="black", elinewidth=.8, capsize=2)
     plt.xlabel( "Alphas")
     plt.ylabel( "Scores")
-    plt.legend()
+    # plt.legend()
+    axes = plt.gca()
+    axes.set_ylim([0, 42000])
+    plt.grid( color="lightgray", linewidth=.5, axis="y")
 
+    # Human expert line
     # plt.boxplot( x=returns, vert=True)
+    # fig5 = plt.figure(5)
+    plt.plot(["0.0", "1.0"], [31212.013,31212.013], color="orange",
+        linewidth=.8, label="Human Avg. Score")
+
     plt.show()
